dff531489d177490d1b7097d843850114a0e14b8
mmc94 <mae.marsha@gmail.com>
2024-05-20 15:25:22 +0800

    Added all calculator features

--- main.js
+++ main.js
@@ -1,46 +1,117 @@
-function add (n1, n2) {
-	return n1 + n2;
-}
+class Calculator {
+	constructor(prevOperandtextElement, currOperandtextElement) {
+		this.prevOperandtextElement = prevOperandtextElement;
+		this.currOperandtextElement = currOperandtextElement;
+		this.clear();
+	}
 
-function subtract (n1, n2) {
-	return n1 - n2;
-}
+	clear() {
+		this.currentOperand = '';
+		this.previousOperand = '';
+		this.operation = undefined;
+	}
 
-function multiply (n1, n2) {
-	return n1 * n2;
-}
+	delete() {
+		if (this.currentOperand == '') return;
+		this.currentOperand = this.currentOperand.slice(0, this.currentOperand.length - 1);
+	}
 
-function divide (n1, n2) {
-	return n1 / n2;
-}
+	appendNumber(number) {
+		if (number === '.' && this.currentOperand.includes('.')) return;
+		this.currentOperand += number;
+	}
 
-function percentage (num) {
-	return num / 100;
-} 
+	chooseOperation(operation) {
+		if (this.currentOperand == '' && this.previousOperand == '') {
+			return;
+		} else if (this.operation == undefined) {
+			this.operation = operation;     
+			if (this.previousOperand == '') {
+				this.previousOperand = this.currentOperand;
+				this.currentOperand = '';
+			} 
+		} else if (this.currentOperand == '' && this.operation != undefined) {
+			this.operation = operation; 
+		} else {
+			this.compute()
+			this.operation = operation;
+		}
+	}
 
-function updateDisplay (btnText) {
-	if (curr.innerText == "") {
+	convertToPercent() {
+		if (this.currentOperand == '' && this.operation == undefined) {
+			this.previousOperand = (parseFloat(this.previousOperand)/ 100).toString()
+		} else if (this.currentOperand != '') {
+			this.currentOperand = (parseFloat(this.currentOperand) / 100).toString()
+		}
+	}
+
+	compute() {
+		if(this.currentOperand == '') return; 
+	
+		const operations = [
+			{operator: '+', opFunc: () => parseFloat(this.previousOperand) + parseFloat(this.currentOperand)},
+			{operator: '-', opFunc: () =>  parseFloat(this.previousOperand) -  parseFloat(this.currentOperand)},
+			{operator: '*', opFunc: () =>  parseFloat(this.previousOperand) *  parseFloat(this.currentOperand)},
+			{operator: '/', opFunc: () =>  parseFloat(this.previousOperand) /  parseFloat(this.currentOperand)},
+		]
+
+		this.previousOperand = operations.find(op => op.operator == this.operation).opFunc().toString();
+		this.operation = undefined;
+		this.currentOperand = '';
 		
 	}
 
-
-	if (numbers.includes(btnText)) {
-		curr.innerText += btnText;
-	} else if (operators.includes(btnText) && curr.innerText != "") {
-		prev.innerText = `${curr.innerText} ${btnText}`;
-		curr.innerText = "";
-	} else {
-		console.log("Others");
+	updateDisplay() {
+		this.currOperandtextElement.innerText = this.currentOperand;
+		this.prevOperandtextElement.innerText = this.previousOperand;
 	}
 
 }
 
-const buttons = document.querySelectorAll('.key-buttons button');
-const prev = document.querySelector('.prev-operand')
-const curr = document.querySelector('.curr-operand')
-const operators = [...document.querySelectorAll('.operator')].map(operator => operator.innerText)
-const numbers = [...document.querySelectorAll('.number')].map(number => number.innerText)
 
-buttons.forEach(button => {
-	button.addEventListener('click',  () => updateDisplay(button.innerText))
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const percentageButton = document.querySelector('[data-percentage]');
+const equalButton = document.querySelector('[data-equal]');
+const prevOperandtextElement = document.querySelector('[data-prev-operand]');
+const currOperandtextElement = document.querySelector('[data-curr-operand]');
+
+const calculator = new Calculator(prevOperandtextElement, currOperandtextElement);
+
+numberButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.appendNumber(button.innerText);
+		calculator.updateDisplay();
+	})
+})
+
+operationButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		calculator.chooseOperation(button.innerText);
+		calculator.updateDisplay();
+	})
+})
+
+equalButton.addEventListener('click', () => {
+	calculator.compute();
+	calculator.updateDisplay();
+})
+
+clearButton.addEventListener('click', ()=> {
+	calculator.clear();
+	calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', ()=> {
+	calculator.delete();
+	calculator.updateDisplay();
+})
+
+percentageButton.addEventListener('click', () => {
+	calculator.convertToPercent();
+	calculator.updateDisplay();
 })
